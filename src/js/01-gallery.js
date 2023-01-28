093e54c7d6c6548fe6909d8273aa0279ec15421e
Denys07748 <denis07748@gmail.com>
2023-01-28 20:29:28 +0200

    додає task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerEl = document.querySelector('.gallery');
+const imegesMarkup = createImegesMarkup(galleryItems);
+
+galleryContainerEl.insertAdjacentHTML('beforeend', imegesMarkup);
+
+function createImegesMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) => `
+  <li>
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  </li>`
+    )
+    .join('');
+}
+
+const SimpleLightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
