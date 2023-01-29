d62778769f2fd1c0b08ac00abaf3dc6ddfb0d709
Denys07748 <denis07748@gmail.com>
2023-01-29 20:00:42 +0200

    додає task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const savedTime = localStorage.getItem('videoplayer-current-time') || 0;
+console.log(savedTime);
+
+const onPlay = data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(savedTime);
